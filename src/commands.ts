8bb3d514bc08a1c24d4c52852dcf948686c19528
marigold <phelixgluy@gmail.com>
2026-06-24 23:11:23 +0600

    add: help command function

---
+++ src/commands.ts
@@ -0,0 +1,6 @@
+export const help = () => {
+	print(`usage:
+ -h\tPrint help --- gpick -h
+ status\tList paths to files with their indexes --- gpick status
+ add\tAdd files of provided indexes to staging area --- gpick add 1 2 3`);
+};
