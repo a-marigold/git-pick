f8ea7459bc83935e540a5f0906f6621d692e61e3
marigold <phelixgluy@gmail.com>
2026-06-25 13:14:42 +0600

    add: logic of entrypoint to commands in index.ts

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import { exit, popen } from 'std';
+
+import { status } from './commands';
+import { GIT_STATUS_COMMAND, HELP_TEXT } from './constants';
+import { getExecFailError } from './utils';
+
+if (scriptArgs.length === 1) {
+	print(HELP_TEXT);
+
+	exit(1);
+}
+
+const command = scriptArgs[0];
+
+if (command === 'status') {
+	const statusText = popen(GIT_STATUS_COMMAND, 'r');
+	if (statusText) {
+		status(statusText.readAsString());
+
+		exit(0);
+	} else {
+		print(getExecFailError(command));
+
+		exit(1);
+	}
+} else if (command === '-h') {
+	print(HELP_TEXT);
+
+	exit(0);
+} else {
+	print('Unknown command.\n\n' + HELP_TEXT);
+	exit(0);
+}
