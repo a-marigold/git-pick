ade6da25af8d33153ddf4b87cd518a74a9e8a434
marigold <phelixgluy@gmail.com>
2026-06-25 12:30:02 +0600

    delete: help command and add constant with help text instead

--- src/commands.ts
+++ src/commands.ts
@@ -1,6 +1,1 @@
-export const help = () => {
-	print(`usage:
- -h\tPrint help --- gpick -h
- status\tList paths to files with their indexes --- gpick status
- add\tAdd files of provided indexes to staging area --- gpick add 1 2 3`);
-};
+import { popen, exit } from 'std';
---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+export const helpText = `usage:
+ -h\tPrint help --- gpick -h
+ status\tList paths to files with their indexes --- gpick status
+ add\tAdd files of provided indexes to staging area --- gpick add 1 2 3`;
+
+export const enum CharCode {
+	'\n' = 10,
+	Space = 32,
+	'\t' = 9,
+
+	'M' = 77,
+
+	'?' = 63,
+}
