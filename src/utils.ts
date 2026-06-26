e1976e971ce6f6817e8744b98e5fb48cd544adfe
marigold <phelixgluy@gmail.com>
2026-06-27 00:08:28 +0600

    delete: getExecFileError util

--- src/utils.ts
+++ src/utils.ts
@@ -1,24 +1,11 @@
-import { pipe } from 'qjs:os';
-
 import { SPAWN_GIT_CMD_OPTIONS } from './constants';
-
-/**
- *
- *
- * @param command Command that failed execution (e.g `git status`)
- *
- * @returns `'Failed to execute: ${command}'`.
- *
- */
-export const getExecFailError = (command: string): `Failed to execute: ${string}` =>
-	('Failed to execute: ' + command) as ReturnType<typeof getExecFailError>;
 
 /**
  * #### Result incldes promise of {@link tjs.Process.wait}, so child process is guaranted to exit.
  *
  * @param command Command with arts to be executed.
  *
- * @returns Tuple where first element is text of `stdout`, second is text of `stderr` and third is {@link tjs.ProcessStatus} of process.
+ * @returns Tuple with promises: where first element is text of `stdout`, second is text of `stderr` and third is {@link tjs.ProcessStatus} of process.
  */
 export const spawnCmd = (command: string[]): Promise<[string, string, tjs.ProcessStatus]> => {
 	const { stdout, stderr, wait } = tjs.spawn(command, SPAWN_GIT_CMD_OPTIONS);
