955e5c2628a3099cfc3e0c3aa36752fb7c5762a5
marigold <phelixgluy@gmail.com>
2026-06-26 20:43:40 +0600

    update: rewrite command parsing logic (index.ts) with txiki api

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,46 @@
-import { exit, popen } from 'std';
-
 import { status } from './commands';
 import { GIT_STATUS_COMMAND, HELP_TEXT } from './constants';
-import { getExecFailError } from './utils';
 
-if (scriptArgs.length === 1) {
-	print(HELP_TEXT);
+const argv = tjs.args;
 
-	exit(1);
+if (argv.length === 1) {
+	console.log(HELP_TEXT);
+
+	tjs.exit(1);
 }
 
-const command = scriptArgs[1];
-
+const command = argv[1];
 if (command === 'status') {
-	const statusText = popen(GIT_STATUS_COMMAND, 'r');
-	if (statusText) {
-		status(statusText.readAsString());
+	const { stdout, stderr, wait } = tjs.spawn(GIT_STATUS_COMMAND, {
+		stdout: 'pipe',
+		stderr: 'pipe',
+	});
 
-		exit(0);
-	} else {
-		print(getExecFailError(command));
+	Promise.all([
+		(stdout as tjs.ProcessReadableStream).text(),
 
-		exit(1);
-	}
+		(stderr as tjs.ProcessReadableStream).text(),
+
+		wait(),
+	]).then((result) => {
+		const error = result[1];
+
+		if (error) {
+			console.log(error);
+
+			tjs.exit(1);
+		} else {
+			const statusResult = status(result[0]);
+
+			console.log(statusResult.output);
+
+			tjs.exit(0);
+		}
+	});
 } else if (command === '-h') {
-	print(HELP_TEXT);
-
-	exit(0);
+	console.log(HELP_TEXT);
+	tjs.exit(0);
 } else {
-	print('Unknown command.\n\n' + HELP_TEXT);
-	exit(1);
+	console.log('Unknown command.\n\n' + HELP_TEXT);
+	tjs.exit(1);
 }
