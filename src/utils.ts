3f6c60ef848a7600949677aa8686bae5f491122e
marigold <phelixgluy@gmail.com>
2026-06-27 00:05:38 +0600

    add: spawnCmd util

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,34 @@
+import { pipe } from 'qjs:os';
+
+import { SPAWN_GIT_CMD_OPTIONS } from './constants';
+
 /**
+ *
+ *
  * @param command Command that failed execution (e.g `git status`)
  *
  * @returns `'Failed to execute: ${command}'`.
+ *
  */
 export const getExecFailError = (command: string): `Failed to execute: ${string}` =>
 	('Failed to execute: ' + command) as ReturnType<typeof getExecFailError>;
+
+/**
+ * #### Result incldes promise of {@link tjs.Process.wait}, so child process is guaranted to exit.
+ *
+ * @param command Command with arts to be executed.
+ *
+ * @returns Tuple where first element is text of `stdout`, second is text of `stderr` and third is {@link tjs.ProcessStatus} of process.
+ */
+export const spawnCmd = (command: string[]): Promise<[string, string, tjs.ProcessStatus]> => {
+	const { stdout, stderr, wait } = tjs.spawn(command, SPAWN_GIT_CMD_OPTIONS);
+
+	// Assertions are not dangerous 'cause `SPAWN_GIT_CMD_OPTIONS` include `stdout` and `stderr`
+	return Promise.all([
+		(stdout as tjs.ProcessReadableStream).text(),
+
+		(stderr as tjs.ProcessReadableStream).text(),
+
+		wait(),
+	]);
+};
