35dc3f8c26b52f3ea4a283add4191aa9f40244fb
marigold <phelixgluy@gmail.com>
2026-06-28 23:08:54 +0600

    update: separate init function from index.ts to init.ts

---
+++ src/init.ts
@@ -0,0 +1,94 @@
+import { exit } from 'std';
+
+import { add, help, status } from './commands';
+import { GIT_GET_DIR_CMD, GIT_STATUS_CMD, STATUS_RESULT_PATH } from './constants';
+import { spawnCmdErr, spawnCmdOut } from './utils';
+/**
+ * @param argv The first arg must be script name.
+ */
+
+export const init = async (argv: string[]): Promise<void> => {
+	if (argv.length === 1) {
+		console.log(help());
+
+		exit(2);
+	}
+	const command = argv[1];
+
+	if (command === 'status') {
+		const gitStatusResult = await spawnCmdOut(GIT_STATUS_CMD);
+		const gitStatusError = gitStatusResult[1];
+
+		if (gitStatusError) {
+			console.log(gitStatusError);
+
+			exit(1);
+		}
+
+		const { paths, output } = status(gitStatusResult[0]);
+
+		const gitDirResult = await spawnCmdOut(GIT_GET_DIR_CMD);
+
+		const gitDirError = gitDirResult[1];
+
+		if (gitDirError) {
+			console.log(gitDirError);
+
+			exit(1);
+		}
+
+		await tjs.writeFile(
+			// slice not to include `\n`
+			gitDirResult[0].slice(0, -1) + STATUS_RESULT_PATH,
+			paths,
+		);
+
+		console.log(output);
+
+		exit(0);
+	}
+
+	if (command === 'add') {
+		const gitDirResult = await spawnCmdOut(GIT_GET_DIR_CMD);
+
+		const gitDirError = gitDirResult[1];
+
+		if (gitDirError) {
+			console.log(gitDirError);
+
+			exit(1);
+		} else {
+			const paths = new TextDecoder()
+				.decode(await tjs.readFile(gitDirResult[0] + STATUS_RESULT_PATH))
+				.split(',');
+
+			const { cmd, error: addError } = add(argv, 2, paths);
+
+			if (addError) {
+				console.log(addError);
+
+				exit(2);
+			} else {
+				const gitAddError = (await spawnCmdErr(cmd))[0];
+
+				if (gitAddError) {
+					console.log(gitAddError);
+
+					exit(1);
+				} else {
+					exit(0);
+				}
+			}
+		}
+	}
+
+	if (command === '-h') {
+		console.log(help());
+
+		exit(0);
+	}
+
+	console.log('Unknown command.\n\n' + help());
+
+	exit(2);
+};
